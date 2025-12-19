718eaa80145336d45a939c03e72eb405bf87642a
烤鱼不吃可乐鸡翅 <146602274+jkllvfree@users.noreply.github.com>
2025-12-19 21:13:24 +0800

    修改了document.js中的api

--- src/api/document.js
+++ src/api/document.js
@@ -2,15 +2,15 @@ import { request } from '@/utils/request';
 
 export const documentService = {
   // 获取文档详情
-  getDetail: (docId) => {
-    return request(`/document/detail/${docId}`);
+  getDetail: (documentId) => {
+    return request(`/document/detail/${documentId}`);
   },
 
   // 更新文档信息 (重命名 或 保存内容)
   updateInfo: ({id, name, content}) => {
     return request('/document/update/info', {
       method: 'POST',
-      body: JSON.stringify({ id, name, content }),
+      body: JSON.stringify({ documentId:id, name, content }),
     });
   }
 };
